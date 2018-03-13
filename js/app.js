ee2156cab070b0fc3e685432f6cb27fc969ee363
Jennifer Bach <jmbach1@gmail.com>
2018-03-12 20:47:53 -0700

    added infor for SEA

--- js/app.js
+++ js/app.js
@@ -1,49 +1,76 @@
-//function to generate random numbers to be used later.
+//Function to calc random number
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-
-var arrayOfRandomCustomersPike = [];
+//Information for Pike location object
 var arrayOfRandomCookiesPike = [];
 var pike = {
   locationName: 'First and Pike',
-  locationHours: [6,7,8],
-  lessCustomers: 23,
+  locationHours: ['6am' ,'7am' ,'8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  minCustomers: 23,
   maxCustomers: 65,
-  randomCustomersPike: function () {
+  avgCookiePerSale: 6.3,
+  //Calc random cookies per hour at location, rounding down if avg sale is not a whole number
+randomCookiesPike: function () {
+  var arrayOfRandomCookiesPike = [];
+  for (i=0; i < this.locationHours.length; i++) {
+    var randomCookies = Math.floor(this.cusstomers * this.avgCookiePerSale);
+  //Push data into an array.
+    arrayOfRandomCookiesPike.push(randomCookies);
 
-    for (i=0; i < this.locationHours.length; i++) {
-      var randomCustomersPike = randomNumber(23, 65);
-      arrayOfRandomCustomersPike.push(randomCustomersPike);
-    }
-   return[arrayOfRandomCustomersPike];
-},
-  randomCookiesPike:  function () {
-  for (i=0; i < this.randomCustomersPike.length; i++) {
-    var randomCookies = Math.floor((this.randomCustomersPike[i]) * 6.3);
-    console.log(randomCookiesPike);
-  arrayOfRandomCookiesPike.push(randomCookies);
+    var cookies = arrayOfRandomCookiesPike;
   }
-  return [arrayOfRandomCookiesPike];
+  return [cookies];
  },
-
-  salesPerHour: function () {
+//Write the sales data onto the page
+  salesPerHourPike: function () {
 
     var pikeContainer = document.getElementById('pike');
-      for(i = 0; i < this.randomCookiesPike.length; i++) {
+      for(i = 0; i < this.locationHours.length; i++) {
 
         var pikeElement = document.createElement('li');
 
-        pikeElement.textContent = this.locationHours[i] + this.randomCookiesPike[i];
+        pikeElement.textContent = this.locationHours[i] + ': ' + this.randomCookiesPike[i] + ' cookies';
         pikeContainer.appendChild(pikeElement);
     
   }
 }
 }
-pike.randomCustomersPike();
 pike.randomCookiesPike();
-pike.salesPerHour();
+pike.salesPerHourPike();
 
 
+//Repeat above steps for SeaTac location
+var arrayOfRandomCookiesSeaTac = [];
+var seaTac = {
+  locationName: 'Seattle-Tacoma International Airport',
+  locationHours: ['6am' ,'7am' ,'8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgCookiePerSale: 1.2,
+  randomCookiesSeaTac: function () {
+    var arrayOfRandomCookiesseaTac = [];
+    for (i=0; i < this.locationHours.length; i++) {
+      var randomCookies = Math.floor(this.cusstomers * this.avgCookiePerSale);
+      arrayOfRandomCookiesSeaTac.push(randomCookies);
 
+      var cookies = arrayOfRandomCookiesSeaTac;
+  }
+  return [cookies];
+ },
+  salesPerHourSeaTac: function () {
+
+    var seaContainer = document.getElementById('sea');
+      for(i = 0; i < this.locationHours.length; i++) {
+
+        var seaElement = document.createElement('li');
+
+        seaElement.textContent = this.locationHours[i] + ': ' + this.randomCookiesSeaTac[i] + ' cookies';
+        seaContainer.appendChild(seaElement);
+    
+  }
+}
+}
+seaTac.randomCookiesSeaTac();
+seaTac.salesPerHourSeaTac();
 
